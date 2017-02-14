be8fbc022315f6c330b98e422e88cde527057d3c
tongVincent <tongvincent@163.com>
2017-02-14 22:07:44 +0800

    add String codePointAt test

--- src/test/java/com/twx/StringTest.java
+++ src/test/java/com/twx/StringTest.java
@@ -38,4 +38,20 @@ public class StringTest extends BaseTest {
     public void test004() {
         MessageUtil.onTime("" + null);
     }
+
+    // 从下面的测试可以知道，如果计算字的个数，可以根据codePointAt的值>0xFFFF来判断这个字占2个字符（4字节）。
+    @Test
+    public void test005() {
+        String str = "\uD842\uDFB7a"; //𠮷,这个字是2个字符的
+        MessageUtil.onTime(str.length());
+        MessageUtil.onTime(str.codePointAt(0)); // 返回的是𠮷的unicode值
+        MessageUtil.onTime(str.codePointAt(1)); // 返回的是𠮷的第二个字符的unicode值
+        MessageUtil.onTime(str.codePointAt(2)); // 返回a的unicode值
+        MessageUtil.onTime(str.charAt(0));
+        MessageUtil.onTime(str.charAt(1));
+        MessageUtil.onTime(str.charAt(2));
+        MessageUtil.onTime((int)str.charAt(0));
+        MessageUtil.onTime((int)str.charAt(1));
+        MessageUtil.onTime((int)str.charAt(2));
+    }
 }
